6fa2c90ef5117bd02ca3dc1d35fa625a881d28ac
howatab <howatabik@gmail.com>
2024-07-22 09:30:44 +0500

    Li2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import './App.css';
 import MovieCard from './MovieCard';
 
 const api_url = 'https://www.omdbapi.com/?apikey=e7a66a56';
-const movie = {
-  "Title": "Italian Spiderman",
-  "Year": "2007",
-  "imdbID": "tt2705436",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
-}
+
 const App = ()=>{
   const[Movies,SetMovies] = useState([])
   const[searchvalue, Setsearchvalue] = useState('')
